4c92bb47d453f07a4798a6de4f5142b5112e4201
Javier Mercedes <manuelmercedez10@gmail.com>
2024-05-19 14:30:01 -0400

    Added default branch name

--- gitCommands.ps1
+++ gitCommands.ps1
@@ -9,7 +9,7 @@ Set-Alias -Name gcb -Value GitCreateBranch
 
 Function GitPull([string]$branch) {
     $CurrentBranch = $(git rev-parse --abbrev-ref HEAD --)[0]
-    if ($null -eq $branch) {
+    if ($branch -eq $null) {
         git pull origin $CurrentBranch
     } else {
         git pull origin $branch
@@ -21,8 +21,8 @@ Function GitAddAll {git add --all}
 Set-Alias -Name gaa -Value GitAddAll
 
 Function GitPush ([string]$branch) {
-    $CurrentBranch = $(git rev-parse --abbrev-ref HEAD --)[0] 
-    if ($null -eq $branch) {
+    $CurrentBranch = $(git rev-parse --abbrev-ref HEAD --)[0]
+    if ($branch -eq $null) {
         git push origin $CurrentBranch
     } else {
         git push origin $branch

d2e4740128e447b8b4a7896624989314caba4bd9
Javier Mercedes <manuelmercedez10@gmail.com>
2024-05-19 14:18:36 -0400

    Added new commands and current git repo

--- gitCommands.ps1
+++ gitCommands.ps1
@@ -8,21 +8,24 @@ Function GitCreateBranch([string]$branch) {git checkout -b $branch}
 Set-Alias -Name gcb -Value GitCreateBranch
 
 Function GitPull([string]$branch) {
+    $CurrentBranch = $(git rev-parse --abbrev-ref HEAD --)[0]
     if ($null -eq $branch) {
-        git pull
+        git pull origin $CurrentBranch
     } else {
         git pull origin $branch
     }
 }
 Set-Alias -Name ggl -Value GitPull
 
-Function GitAddAll {git add -all}
+Function GitAddAll {git add --all}
 Set-Alias -Name gaa -Value GitAddAll
 
 Function GitPush ([string]$branch) {
+    $CurrentBranch = $(git rev-parse --abbrev-ref HEAD --)[0] 
     if ($null -eq $branch) {
-        git push
+        git push origin $CurrentBranch
     } else {
         git push origin $branch
     }
 }
+Set-Alias -Name ggp -Value GitPush
